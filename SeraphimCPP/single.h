277123f13fd0bc7da0a6e15c7d3b98454e9a75be
dongjinbin <td.seraphim@gmail.com>
2013-03-02 15:54:47 +0800

    seraphim

---
+++ SeraphimCPP/single.h
@@ -0,0 +1,14 @@
+#pragma once
+namespace seraphim{
+class single
+{
+int d;
+single* next;
+public:
+	single(int _d,single* _next){d=_d;next=_next;}
+	int getData(){return d;}
+	single* getNext(){return next;}
+	void setNext(single* _next){this->next = _next;}
+	~single(void);
+};
+};
