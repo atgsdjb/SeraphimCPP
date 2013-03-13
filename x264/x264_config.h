3a27b709088241db89b9af4a7d67edbeaf318112
dongjb <td.seraphim@gmail.com>
2013-03-13 17:46:20 +0800

    break

--- x264/x264_config.h
+++ x264/x264_config.h
@@ -1,12 +1,6 @@
-#ifndef __X264_CONFIG_H__
-#define __X264_CONFIG_H__
-
 #define X264_BIT_DEPTH     8
 #define X264_GPL           1
 #define X264_INTERLACED    1
 #define X264_CHROMA_FORMAT 0
 #define X264_VERSION ""
-#define X264_POINTVER "0.125.x"
-#define X264_API_IMPORTS
-
-#endif // __X264_CONFIG_H__
+#define X264_POINTVER "0.130.x"
